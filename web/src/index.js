7989ebf80a01b1c2cf3920d4f85d49f5b72a90c3
ryyan <ryyan@pm.me>
2020-07-26 14:57:45 -0500

    Move encryption form into own component

--- web/src/index.js
+++ web/src/index.js
@@ -1,6 +1,8 @@
-import reload from '@riotjs/hot-reload'
-import * as riot from 'riot'
-import App from './app.riot'
+import reload from "@riotjs/hot-reload";
+import * as riot from "riot";
+import App from "./app.riot";
+import Encrypt from "./encrypt.riot";
 
-riot.register('app', App)
-riot.mount('app')
+riot.register("app", App)
+riot.register("encrypt", Encrypt)
+riot.mount("app")
