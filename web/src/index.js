206ab872bc51b08f861a772847584204d988e39d
ryyan <ryyan@pm.me>
2020-07-26 17:10:12 -0500

    Add decrypt component and simple routing

--- web/src/index.js
+++ web/src/index.js
@@ -2,7 +2,9 @@ import reload from "@riotjs/hot-reload";
 import * as riot from "riot";
 import App from "./app.riot";
 import Encrypt from "./encrypt.riot";
+import Decrypt from "./decrypt.riot";
 
-riot.register("app", App)
-riot.register("encrypt", Encrypt)
-riot.mount("app")
+riot.register("app", App);
+riot.register("encrypt", Encrypt);
+riot.register("decrypt", Decrypt);
+riot.mount("app");
